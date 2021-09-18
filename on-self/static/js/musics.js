6e29f4a72f7892841b644d5fdb43c55af08df589
shiramashiro <zhengrenfu@outlook.com>
2021-09-19 01:11:04 +0800

    分割数据

---
+++ on-self/static/js/musics.js
@@ -0,0 +1,33 @@
+let musicsDb = [
+    {
+        name: '秋绪',
+        album: '赛壬唱片-MSR',
+        lyrics: `Oh, Darling !<br/>哦！亲爱的！<br/>Don't you know ? Don't you know life's so sweet ?<br/>难道你不知道？难道你不知道这生活有多么的美妙吗？<br/>きみの夢　どういうの？<br/>你的梦到底会是怎样的呢？<br/>その瞳をのぞけば　明日がSay Hello !<br/>若眺望着那双眼眸，明日便会来迎接你！<br/>笑ってる　Life is so sweet<br/>不禁让人流露出笑容，生活竟是如此的美妙！<br/>虹が見えたり　星　流れたり我仰望着彩虹，空中流星闪过.<br/>不意に世界は　輝きだすね<br/>在不经意间世界闪耀着光芒.<br/>だから Don't give up　くじけないで　そう！ We are dreamers !<br/>所以请不要放弃，不必感到气馁 ，是啊！正因为我们是追梦者！<br/>You can change 唄いながら　未来　作ろう<br/>“你可以改变这一切！”我如此低语着 ，一起来开创这未来吧！<br/>
+Oh, Darling !<br/>哦！亲爱的！<br/>Don't you know ? Don't you know life's so sweet ?<br/>难道你不知道？难道你不知道这生活有多么的美妙吗？<br/>きみの夢　どういうの？<br/>你的梦到底会是怎样的呢？<br/>その瞳をのぞけば　明日がSay Hello !<br/>若眺望着那双眼眸，明日便会来迎接你.<br/>Yes, Darling !<br/>是的！亲爱的！<br/>Don't you know ? Don't you know ? 奇跡と待ち合わせしようよ<br/>难道你不知道吗？难道你不知道吗？和奇迹来一场约会吧！<br/>なんでもないところに　愛がWaits for you !<br/>在空无一物的地方，将会有爱等候着你.<br/>あふれてる　Life is so sweet<br/>这满溢而出的幸福，生活竟是如此的美妙！<br/>`,
+        musicSrc: 'https://generic-data.oss-cn-chengdu.aliyuncs.com/img/training/music/item01/%E7%A7%8B%E7%BB%AA.mp3',
+        surface: 'https://p2.music.126.net/AuJtKCWhFMIE17igpXBPPA==/109951165379323215.jpg?param=130y130'
+    },
+    {
+        name: 'I really ∞',
+        album: '我真的真的真的真的',
+        lyrics: `I really<br/>我真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>Late night watching television<br/>在深夜看电视<br/>But how we get in this position?<br/>但我们是怎么进入这种气氛的呢？<br/>It's way too soon, I know this isn't love<br/>太早了，我知道这不是爱情<br/>But I need to tell you something<br/>但我得告诉你一件事<br/>I really<br/>我真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>真的<br/>really<br/>Late night watching television<br/>在深夜看电视<br/>But how we get in this position?<br/>但我们是怎么进入这种气氛的呢？<br/>It's way too soon, I know this isn't love<br/>太早了，我知道这不是爱情<br/>But I need to tell you something<br/>但我得告诉你一件事<br/>I really really really really really really<br/>我真的 真的 真的 真的 真的 真的<br/>like you<br/>喜欢你<br/>And I want you<br/>我想你<br/>do you want me,<br/>你想我吗？<br/>do you want me, too?<br/>你也一样想我吗？<br/>`,
+        musicSrc: 'https://generic-data.oss-cn-chengdu.aliyuncs.com/img/training/music/item02/I%20really%20%E2%88%9E.mp3',
+        surface: 'https://p2.music.126.net/DwWVQH2lsnqJEpeUiNaCZA==/109951164416460276.jpg?param=130y130'
+    },
+    {
+        name: 'Splashing Star',
+        album: '崩坏3-Alive-Original Soundtrack',
+        lyrics: `存音乐，请尽情欣赏`,
+        musicSrc:
+            'https://generic-data.oss-cn-chengdu.aliyuncs.com/img/training/music/item03/HOYO-MiX%20-%20Splashing%20Star.mp3',
+        surface: 'https://p1.music.126.net/eo1oYY1OQobITrvSNf_nrA==/109951165705882625.jpg?param=130y130'
+    },
+    {
+        name: '壶中洞天',
+        album: '陈致逸 / HOYO-MiX /《原神》璃月篇OST',
+        lyrics: `存音乐，请尽情欣赏`,
+        musicSrc:
+            'https://generic-data.oss-cn-chengdu.aliyuncs.com/img/training/music/item04/%E5%A3%B6%E4%B8%AD%E6%B4%9E%E5%A4%A9.mp3',
+        surface: 'https://p1.music.126.net/yoRaxBY77koSqhjh52g-DA==/109951165434255510.jpg?param=130y130'
+    }
+]
